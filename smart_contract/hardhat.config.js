7f3c315674546400c6609bc1a43b93c03a3b74f8
Rabeeb Aqdas Jilani <63879512+Rabeebaqdas@users.noreply.github.com>
2023-02-01 18:05:17 +0500

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks : {
     goerli : {
       url :"https://eth-goerli.g.alchemy.com/v2/k2GSNrRmvJt3TFO5tRTwZ6u1rH4dw0CR", // alchemy url for the deployment of smart contract
-      accounts : ['5a7b4f248dbce5d069b17e8db571eb325c5e9113dead123fc1cbe8ef9a1351f4'] //private key
+      accounts : [''] //private key
     }
   }
 }
